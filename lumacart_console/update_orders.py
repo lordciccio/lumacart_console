11493577e15cdeb133a72573f900eaf4e07add7f
lordiccio <zetacode@gmail.com>
2016-01-18 18:42:58 +0100

    log

--- lumacart_console/update_orders.py
+++ lumacart_console/update_orders.py
@@ -2,7 +2,7 @@
 
 import os
 import sys
-
+import logging
 import django
 
 project_home = u'/home/lumacart/projects/lumacart_console'
@@ -14,10 +14,12 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'lumacart_console.prod_settings'
 os.environ['C2O_API_KEY'] = '6NHMoQYdOHbjBIZEwNe4cliHQ50fGHNNvvzUmy7afjtR1svxgKCs6V9W1abHRZvn'
 
 django.setup()
+logger = logging.getLogger("project")
 
 # add your project directory to the sys.path
 from lumacart_console.orders.routines import update_order_status
 from lumacart_console.orders.models import C2OOrder
 
+logger.info("Checking orders status...")
 for order in C2OOrder.objects.filter(status__in = [C2OOrder.STATUS_SENT]):
     update_order_status(order)
