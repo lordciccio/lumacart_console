cfd2f5e50c6f2329fd88f9dd7bee59fdf4f7d27c
lordciccio <zetacode@gmail.com>
2016-03-07 08:16:59 +0000

    added woocommerce hooks

--- lumacart_console/urls.py
+++ lumacart_console/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^catalogue/', include("lumacart_console.catalogue.urls")),
     url(r'^orders/', include("lumacart_console.orders.urls")),
+    url(r'^hooks/', include("lumacart_console.hooks.urls")),
     url(r'^', include("lumacart_console.dashboard.urls")),
 ]
